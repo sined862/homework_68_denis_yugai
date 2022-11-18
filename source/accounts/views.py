77c3cf2b14c6aa2e4fa5c7193cd190d730d622cb
Yugai Denis <yugai_denis@mail.ru>
2022-11-19 00:00:48 +0600

    #2 Добавление базового шаблона и удаление тестовых файлов

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -7,9 +7,10 @@ from django.urls import reverse
 from django.contrib.auth.views import LoginView
 
 class RegisterView(CreateView):
-    template_name = 'register.html'
+    template_name = 'accounts/register.html'
     form_class = CustomUserCreationForm
     success_url = '/'
+    extra_context = {'title': 'Регистрация пользователя'}
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST, request.FILES)
@@ -24,34 +25,7 @@ class RegisterView(CreateView):
 
 class LoginUserView(LoginView):
     form_class = LoginUserForm
-    template_name = 'login.html'
+    template_name = 'accounts/login.html'
+    extra_context = {'title': 'Авторизация пользователя'}
 
-# class LoginView(TemplateView):
-#     template_name = 'login.html'
-#     form = LoginForm
-
-#     def get(self, request, *args, **kwargs):
-#         next = request.GET.get('next')
-#         form_data = {} if not next else {'next': next}
-#         form = self.form(form_data)
-#         context = {'form': form}
-#         return self.render_to_response(context)
-
-#     def post(self, request, *args, **kwargs):
-#         form = self.form(request.POST)
-#         if not form.is_valid():
-#             return redirect('login')
-#         username = form.cleaned_data.get('username')
-#         password = form.cleaned_data.get('password')
-#         next = form.cleaned_data.get('next')
-#         user = authenticate(request, username=username, password=password)
-#         if not user:
-#             return redirect('login')
-#         login(request, user)
-#         if next:
-#             return redirect(next)
-#         return redirect('profile')
-
-class ProfileView(TemplateView):
-    template_name = 'profile.html'
 
